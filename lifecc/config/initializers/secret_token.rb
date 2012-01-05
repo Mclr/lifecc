56e46a1ab7fe87ca846ebc81787df93a8df823c9
Matt CR <mclr@ymail.com>
2012-01-05 10:42:43 -0800

    First Commitment

---
+++ lifecc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifecc::Application.config.secret_token = '7fa5778bc225235973044ef672dc270afe56435181e98ae1a6089b7c8dc088dc18c6c9e48ada6502c3d9294605915b2ba30c7abb1564c402d379f7dd4b99e881'
